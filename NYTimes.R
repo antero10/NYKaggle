34f78322b4b4b9c09ae219fe2f80feeaf0c2ce32
Gustavo Sanchez <antero105@gmail.com>
2015-05-02 19:57:41 -0300

    Creating CART model from NewsDesk, SectionName, SubSectionName to fill out white spaces

--- NYTimes.R
+++ NYTimes.R
@@ -6,6 +6,8 @@ library(rpart.plot)
 library(randomForest)
 library(tm)
 library(plyr)
+library(caret)
+library(e1071)
 
 #Read the data 
 NewsTrain = read.csv("NYTimesBlogTrain.csv", stringsAsFactors=FALSE)
@@ -14,6 +16,9 @@ NewsTrain$PubDate = strptime(NewsTrain$PubDate, "%Y-%m-%d %H:%M:%S")
 NewsTest$PubDate = strptime(NewsTest$PubDate, "%Y-%m-%d %H:%M:%S")
 NewsTrain$Hour = NewsTrain$PubDate$hour
 NewsTest$Hour = NewsTest$PubDate$hour
+NewsTrain$Minute = NewsTrain$PubDate$min
+NewsTest$Minute = NewsTest$PubDate$min
+#NewsTest$Weekday = factor(weekdays(as.Date(NewsTrain$PubDate)))
 NewsTrain2 = NewsTrain
 NewsTest2 = NewsTest
 
@@ -32,7 +37,7 @@ CorpusHeadline = tm_map(CorpusHeadline, tolower)
 CorpusHeadline = tm_map(CorpusHeadline, PlainTextDocument)
 CorpusHeadline = tm_map(CorpusHeadline, removePunctuation)
 CorpusHeadline = tm_map(CorpusHeadline, removeWords, stopwords("english"))
-CorpusHeadline = tm_map(CorpusHeadline, stemDocument)
+CorpusHeadline = tm_map(CorpusHeadline, stemDocument,language="english")
 dtmHeadline = DocumentTermMatrix(CorpusHeadline)
 sparseHeadline = removeSparseTerms(dtmHeadline, 0.99)
 HeadlineWords = as.data.frame(as.matrix(sparseHeadline))
@@ -44,7 +49,7 @@ CorpusAbstract = tm_map(CorpusAbstract, tolower)
 CorpusAbstract = tm_map(CorpusAbstract, PlainTextDocument)
 CorpusAbstract = tm_map(CorpusAbstract, removePunctuation)
 CorpusAbstract = tm_map(CorpusAbstract, removeWords, stopwords("english"))
-CorpusAbstract = tm_map(CorpusAbstract, stemDocument)
+CorpusAbstract = tm_map(CorpusAbstract, stemDocument,language="english")
 dtmAbstract = DocumentTermMatrix(CorpusAbstract)
 sparseAbstract = removeSparseTerms(dtmAbstract, 0.99)
 AbstractWords = as.data.frame(as.matrix(sparseAbstract))
@@ -57,7 +62,7 @@ CorpusSnippet = tm_map(CorpusSnippet, tolower)
 CorpusSnippet = tm_map(CorpusSnippet, PlainTextDocument)
 CorpusSnippet = tm_map(CorpusSnippet, removePunctuation)
 CorpusSnippet = tm_map(CorpusSnippet, removeWords, stopwords("english"))
-CorpusSnippet = tm_map(CorpusSnippet, stemDocument)
+CorpusSnippet = tm_map(CorpusSnippet, stemDocument,language="english")
 dtmSnippet = DocumentTermMatrix(CorpusSnippet)
 sparseSnippet = removeSparseTerms(dtmSnippet, 0.99)
 SnippetWords = as.data.frame(as.matrix(sparseSnippet))
@@ -72,29 +77,53 @@ dtm = cbind(HeadlineWords, AbstractWords,SnippetWords)
 dtm$Test = News$Test
 dtm$Popular = News$Popular
 dtm$WordCount = News$WordCount
+dtm$NewsDesk = as.factor(News$NewsDesk)
+dtm$SectionName = as.factor(News$SectionName)
+dtm$SubsectionName = as.factor(News$SubsectionName)
+dtm$Hour = News$Hour
 
-
-
+dtm$UniqueID = News$UniqueID
 #Split
 train = subset(dtm,Test == 0)
 test = subset(dtm,Test == 1)
 test$Popular = NULL
+#numFolds = trainControl(method = "cv",number=10)
+#cpGrid = expand.grid(.cp=seq(0.01,0.5,0.01))
+#train(Popular ~ ., data=train,method="rpart",trControl = numFolds,tuneGrid = cpGrid)
 
+#Submision
+#ExamCART = rpart(Popular ~ ., data=train,method="class",cp=0.01)
+#ExamPredictCART = predict(ExamCART,newdata=test,type="class")
+#ExamRandomForest = randomForest(Popular ~ ., data=train)
+#ExamPredictRandomForest = predict(ExamRandomForest,newdata=test)
+#MySubmission = data.frame(UniqueID = test$UniqueID, Probability1 = ExamPredictRandomForest)
+#write.csv(MySubmission, "SubmissionSimpleLog.csv", row.names=FALSE)
+
+
+#Machine learning
 #For some test purpouse
 
 set.seed(144)
 split = sample.split(train$Popular, SplitRatio = 0.7)
 train2 = subset(train, split==TRUE)
 test2 = subset(train,split == FALSE)
-
-#Machine learning
-
 #Logistic Regression
 
-NewsLog = glm(Popular ~ ., data = train2, family = "binomial")
-PredictLog = predict(NewsLog,newdata=test2,type="response")
+#NewsLog = glm(Popular ~ ., data = train2, family = 'binomial')
+#PredictLog = predict(NewsLog,newdata=test2,type='response')
 
 #CART
 
-NewsCART = rpart(Popular ~ ., data= train2, method="class")
-PredictCART = predict(NewsCART,newdata=test2,type="class")
+NewsDeskCART = rpart(NewsDesk ~ .,data=train2,method='class')
+predictDeskCART = predict(NewsDeskCART,newdata=test2,type="class")
+NewsSectionNameCART = rpart(SectionName ~.,data=train2,method='class')
+predictSectionNameCART = predict(NewsSectionNameCART,newdata=test2,type='class')
+NewsSubSectionCART = rpart(SubsectionName ~.,data=train2,method='class')
+predictSubSectionCART = predict(NewsSubSectionCART,data=train2,type="class")
+#NewsCART = rpart(Popular ~ ., data= train2, method='class')
+#PredictCART = predict(NewsCART,newdata=test2,type='class')
+
+#Random Forest
+
+#NewsRandomForest = randomForest(Popular ~ ., data= train2)
+#predictForest = predict(NewsRandomForest,newdata=test2)
